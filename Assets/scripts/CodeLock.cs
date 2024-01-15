003ab86f79a63fa019acde1418146836a5ba2bdf
vezamm <zdsellami@gmail.com>
2024-01-15 02:09:33 +0100

    door animation and code lock

---
+++ Assets/scripts/CodeLock.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CodeLock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private string expectedpasswords;
+    [SerializeField] UnityEvent passwordcorrect;
+    [SerializeField] UnityEvent<string> oncurrentpasswordchange;
+    [SerializeField] UnityEvent passwordwrong;
+    [SerializeField] UnityEvent ontrigger;
+    private string Currentpassword;
+    public GameObject text;
+    public GameObject canvas;
+
+
+    public void EnterpasswordText(string text)
+    {
+        if (Currentpassword.Length < expectedpasswords.Length)
+        {
+            Currentpassword += text;
+            oncurrentpasswordchange?.Invoke(Currentpassword);
+            CheckPassword();
+        }
+    }
+    public void ResetPassword()
+    {
+        Currentpassword = "";
+        oncurrentpasswordchange?.Invoke(Currentpassword);
+    }
+    private void CheckPassword()
+    {
+        if (Currentpassword == expectedpasswords)
+        {             
+            passwordcorrect?.Invoke();
+            Cursor.lockState = CursorLockMode.Locked;
+            canvas.SetActive(false);
+            text.SetActive(false);
+        }
+        else if (Currentpassword.Length == expectedpasswords.Length)
+        {
+            passwordwrong?.Invoke();
+            //ResetPassword();
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // text.SetActive(true); ;
+             if(Input.GetKeyDown(KeyCode.E))
+            {
+               canvas.SetActive(true);
+               Cursor.lockState = CursorLockMode.None;
+            }
+            //ontrigger?.Invoke();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canvas.SetActive(false);
+        }
+    }
+
+}
---
+++ Assets/scripts/bloody_door.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bloody_door : MonoBehaviour
+{
+    private Animator animator;
+    public GameObject intText;
+    public bool opened;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>(); 
+        opened = animator.GetBool("open");
+    }
+    private void Update()
+    {
+        opened = animator.GetBool("open");
+    }
+
+    // Update is called once per frame
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {  
+            intText.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (opened == false)
+                {
+                    animator.SetBool("open", true);
+                }
+                else animator.SetBool("open", false);
+                
+            }
+        }
+        
+               
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            intText.SetActive(false);
+        }
+    }
+
+}
+
